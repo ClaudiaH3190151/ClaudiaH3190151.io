079e8a3b08cc97f389852bb2cd59f72aa8496dac
ClaudiaH3190151 <3190151@usap.edu>
2026-08-17 20:53:42 -0600

    Delete Herrera_Claudia_galeria/Herrera_Claudia_galeria directory

--- Herrera_Claudia_galeria/Herrera_Claudia_galeria/js/script.js
+++
@@ -1,9 +0,0 @@
-function agregarCarrito(b)
-{let t=b.textContent;
-    b.textContent='✔ Agregado';
-    b.style.background='#28a745';setTimeout(()=>{
-        b.textContent=t;
-        b.style.background='#ff2d7a';
-    },
-    1500);
-}
